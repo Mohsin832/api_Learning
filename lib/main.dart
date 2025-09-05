797ee06e2ab808278182e4d0fc4d49672d7e2d48
Mohsin832 <mm8116900@gmail.com>
2025-09-05 02:33:25 -0700

    api learning

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:apilearning/apiapp.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
